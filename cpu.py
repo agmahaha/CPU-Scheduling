3669fc8a184ed7fb80f3a874301303001b671e26
agmahaha <antonio_mendoza@dlsu.edu.ph>
2024-02-29 23:08:13 +0800

    working srtf algo but wrong waiting time and print

--- cpu.py
+++ cpu.py
@@ -3,6 +3,7 @@ def fcfs(processes):
     processes.sort(key=lambda x: x[1]) # Sort by arrival time
     current_time = 0
     waiting_times = []
+
     for pid, arrival_time, burst_time in processes:
         if arrival_time <= current_time:
             start_time = current_time
@@ -19,16 +20,20 @@ def sjf(processes):
     processes.sort(key=lambda x: (x[1], x[2]))
     current_time = 0
     waiting_times = []
+
     while processes:
         shortest_process = None
         for process in processes:
             pid, arrival_time, burst_time = process
+
             if arrival_time <= current_time:
                 if shortest_process is None or burst_time < shortest_process[2]:
                     shortest_process = process
+
         if shortest_process is None:
             current_time += 1
             continue
+
         # Execute the shortest process
         pid, arrival_time, burst_time = shortest_process
         start_time = current_time
@@ -36,11 +41,57 @@ def sjf(processes):
         waiting_time = start_time - arrival_time
         waiting_times.append((pid, start_time, end_time, waiting_time))
         current_time = end_time
+
         # Remove the executed process from the list
         processes.remove(shortest_process)
     return waiting_times
-def sorter(processes, n):
-    return sorted(processes, key=lambda x: x[n])
+
+def srtf(processes):
+    # Sort processes by arrival time and then by burst time
+    current_time = 0
+    waiting_times = []
+    start_time = 0
+    end_time = 0
+
+    while processes:
+        available_process = []
+        for process in processes:
+            arrival_time = process[1]
+
+            if arrival_time <= current_time:
+                available_process.append(process)
+
+        if available_process == []:
+            current_time += 1
+            
+            continue
+
+        current_time += 1
+        available_process.sort(key=lambda x: (x[2], x[1]))
+        available = available_process[0]
+        updated_process = list(available)
+        print(updated_process)
+        updated_process[2] -= 1
+        
+        
+        
+
+        if updated_process[2] == 0:
+            start_time = end_time
+            processes.remove(available)
+            pid, arrival_time, burst_time = available
+            end_time = current_time
+            waiting_time = end_time - arrival_time - burst_time
+            waiting_times.append((pid, start_time , end_time, waiting_time))
+        else:
+            processes.remove(available)
+            processes.append(updated_process)
+
+        
+
+                
+    return waiting_times
+
 
 filename = input("Enter Filename: ")
 
@@ -55,10 +106,13 @@ try:
         processes = [list(map(int, line.split())) for line in contents[1:]]
 
         # Choose the scheduling algorithm based on the input
+
         if algorithm == 0:
             waiting_times = fcfs(processes)
         elif algorithm == 1:
             waiting_times = sjf(processes)
+        elif algorithm == 2:
+            waiting_times = srtf(processes)
         # Add other scheduling algorithms here if needed
 
         # Print the waiting times for each process
